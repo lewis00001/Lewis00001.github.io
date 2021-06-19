32ab29ffe91a8b6a5b754f884caa2a2f90264a10
Lewis Lockhart <lewis00001@gmail.com>
2021-06-19 17:56:47 -0600

    adding completed week 9 assignment

--- lesson9/js/newsApi.js
+++ lesson9/js/newsApi.js
@@ -6,26 +6,24 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
 
-    // holds the news artical array
+    // holds the news article array
     const news = jsonObject.response.docs;
     // gets the length of the news array for the forLoop
     let length = Object.keys(jsonObject.response.docs).length;
 
     for (let i = 0; i < length; i++ ) {
       
-      console.log(news[i]);
-      
       // create html elements
-      let articalDiv = document.createElement('div');
+      let articleDiv = document.createElement('div');
       let headline = document.createElement('h2');
       let snippet = document.createElement('p');
       let url = document.createElement('a');
       
       // add classes
-      articalDiv.classList.add('home-artical-div');
+      articleDiv.classList.add('home-article-div');
       headline.classList.add('home-headline');
       snippet.classList.add('home-p');
-      url.classList.add('home-artical-url');
+      url.classList.add('home-article-url');
 
       // add content
       headline.textContent = news[i].headline.main;
@@ -34,9 +32,10 @@ fetch(requestURL)
       url.target = '_blank';
       url.textContent = 'Read More ...';
 
-      articalDiv.appendChild(headline);
-      articalDiv.appendChild(snippet).appendChild(url);
-      document.querySelector('.home-news').appendChild(articalDiv);
+      // output content to the page
+      articleDiv.appendChild(headline);
+      articleDiv.appendChild(snippet).appendChild(url);
+      document.querySelector('.home-news').appendChild(articleDiv);
   }
 
 });
